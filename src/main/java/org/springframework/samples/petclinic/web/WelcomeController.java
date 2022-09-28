7bfae9576b88388004517afb26aeafa36c72a100
Sergio Santiago <sergiosantiago0403@gmail.com>
2022-09-28 16:27:29 +0200

    for each

--- src/main/java/org/springframework/samples/petclinic/web/WelcomeController.java
+++ src/main/java/org/springframework/samples/petclinic/web/WelcomeController.java
@@ -1,7 +1,10 @@
 package org.springframework.samples.petclinic.web;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
+import org.springframework.samples.petclinic.model.Person;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -10,7 +13,15 @@ public class WelcomeController {
 	
 	
 	  @GetMapping({"/","/welcome"})
-	  public String welcome(Map<String, Object> model) {	    
+	  public String welcome(Map<String, Object> model) {	
+		List<Person> persons = new ArrayList<Person>();
+		Person sergio = new Person();
+		sergio.setFirstName("Sergio"); 
+		sergio.setLastName("Santiago");
+		persons.add(sergio);
+		model.put("persons", persons);
+		model.put("title", "L3-3");
+		model.put("group", "G2 L3-3");
 
 	    return "welcome";
 	  }
