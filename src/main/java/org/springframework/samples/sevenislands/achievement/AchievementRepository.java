4545c892d274a532283d478a2e01ba98b72ce877
Sergio Santiago <sergiosantiago0403@gmail.com>
2022-10-27 00:26:42 +0200

    service card, lobby v1

--- src/main/java/org/springframework/samples/sevenislands/achievement/AchievementRepository.java
+++ src/main/java/org/springframework/samples/sevenislands/achievement/AchievementRepository.java
@@ -1,5 +1,16 @@
 package org.springframework.samples.sevenislands.achievement;
 
-import org.springframework.data.repository.CrudRepository;
+import java.util.Collection;
 
-public interface AchievementRepository extends CrudRepository<Achievement, Integer>{}
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+
+public interface AchievementRepository extends CrudRepository<Achievement, Integer>{
+
+    @Query("Select achievement from Achievement achievement where achievement.achievementType = :achievementType")
+    Collection<Achievement> findByType(@Param("achievementType") AchievementType achievementType);
+
+    
+}
--- src/main/java/org/springframework/samples/sevenislands/achievement/AchievementService.java
+++ src/main/java/org/springframework/samples/sevenislands/achievement/AchievementService.java
@@ -1,5 +1,31 @@
 package org.springframework.samples.sevenislands.achievement;
 
+import java.util.Collection;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AchievementService {
     
+    @Autowired
+    private AchievementRepository achievementRepository;
+
+    @Transactional
+    public Achievement getAchievementById(Integer id){
+        return achievementRepository.findById(id).orElse(null);
+    }
+
+    @Transactional
+    public void save(Achievement achievement){
+        achievementRepository.save(achievement);
+    }
+
+    @Transactional
+    public Collection<Achievement> getAchievementByType(AchievementType achievementType){
+        return achievementRepository.findByType(achievementType);
+    }
+
 }
