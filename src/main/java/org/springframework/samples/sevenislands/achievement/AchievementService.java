753b1c00e9a640a7664562b485b1d80b82d747b8
alepervaz <alepervaz@us.es>
2022-11-05 16:14:38 +0100

    subida entidad achievement

---
+++ src/main/java/org/springframework/samples/sevenislands/achievement/AchievementService.java
@@ -0,0 +1,41 @@
+package org.springframework.samples.sevenislands.achievement;
+
+import java.util.Collection;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class AchievementService {
+ 
+    private AchievementRepository achievementRepository;
+
+    @Autowired
+	public AchievementService(AchievementRepository achievementRepository) {
+		this.achievementRepository = achievementRepository;
+	}		
+
+	@Transactional(readOnly = true)	
+	public Iterable<Achievement> findAchievements() throws DataAccessException {
+		return achievementRepository.findAll();
+	}
+
+    @Transactional(readOnly = true)
+	public Optional<Achievement> findAchievementById(int id) throws DataAccessException {
+		return achievementRepository.findById(id);
+	}
+
+    @Transactional
+	public void saveAchievement(Achievement achievement) throws DataAccessException {
+		achievementRepository.save(achievement);
+	}
+    
+    @Transactional
+    public Collection<Achievement> getAchievementByType(AchievementType achievementType) {
+        return achievementRepository.findByType(achievementType);
+    }
+
+}
