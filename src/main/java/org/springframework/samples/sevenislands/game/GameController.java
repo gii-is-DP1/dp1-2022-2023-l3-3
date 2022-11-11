539fd26c1b85ff4d3d85df3ec4f2c5ae4f913421
albperleo <albperleo@alum.us.es>
2022-11-11 09:33:59 +0100

    Comenzando con los métodos del game controller para las acciones de partida

--- src/main/java/org/springframework/samples/sevenislands/game/GameController.java
+++ src/main/java/org/springframework/samples/sevenislands/game/GameController.java
@@ -5,6 +5,8 @@ import java.time.LocalDateTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.samples.sevenislands.card.CardService;
+import org.springframework.samples.sevenislands.game.island.IslandService;
+import org.springframework.samples.sevenislands.game.round.RoundService;
 import org.springframework.samples.sevenislands.game.turn.Turn;
 import org.springframework.samples.sevenislands.game.turn.TurnService;
 import org.springframework.samples.sevenislands.lobby.Lobby;
@@ -24,21 +26,31 @@ public class GameController {
     private final GameService gameService;
     private final PlayerService playerService;
     private final LobbyService lobbyService;
+    private final RoundService roundService;
     private final TurnService turnService;
+    private final IslandService islandService;
     private final CardService cardService;
 
     @Autowired
     public GameController(GameService gameService,
             PlayerService playerService,
             LobbyService lobbyService,
+            RoundService roundService,
             TurnService turnService,
+            IslandService islandService,
             CardService cardService) {
         this.gameService = gameService;
         this.playerService = playerService;
         this.lobbyService = lobbyService;
+        this.roundService = roundService;
         this.turnService = turnService;
+        this.islandService = islandService;
         this.cardService = cardService;
     }
+
+    ////////////////////////////////////////////////////////////////////////////
+    /// MÉTODOS PARA LA CREACIÓN DEL JUEGO ///
+    ////////////////////////////////////////////////////////////////////////////
 
     @GetMapping("/game")
     public ModelAndView Game(Principal principal) {
@@ -46,7 +58,23 @@ public class GameController {
         return result;
     }
 
-    //
+    @GetMapping("/game/create")
+    public String createGame(Principal principal) {
+        Game game = new Game();
+        Player player = playerService.findPlayersByName(principal.getName());
+        Lobby lobby = lobbyService.findLobbyByPlayer(player.getId());
+        game.setCreationDate(LocalDateTime.now());
+        game.setLobby(lobby);
+        gameService.save(game);
+        lobby.setGame(game);
+        lobbyService.update(lobby);
+        return "redirect:/game/turn";
+    }
+
+    ////////////////////////////////////////////////////////////////////////////
+    /// MÉTODOS PARA LA ASIGNACIÓN DE TURNOS ///
+    ////////////////////////////////////////////////////////////////////////////
+
     @GetMapping("/game/AsignTurn")
     public String GameAsignTurn(Principal principal) {
         Player player = playerService.findPlayersByName(principal.getName());
@@ -65,17 +93,18 @@ public class GameController {
         return VIEWS_GAMETURN;
     }
 
-    @GetMapping("/game/create")
-    public String createGame(Principal principal) {
-        Game game = new Game();
-        Player player = playerService.findPlayersByName(principal.getName());
-        Lobby lobby = lobbyService.findLobbyByPlayer(player.getId());
-        game.setCreationDate(LocalDateTime.now());
-        game.setLobby(lobby);
-        gameService.save(game);
-        lobby.setGame(game);
-        lobbyService.update(lobby);
-        return "redirect:/game/turn";
-    }
+    ////////////////////////////////////////////////////////////////////////////
+    /// MÉTODOS PARA ACCIONES DEL JUEGO ///
+    ////////////////////////////////////////////////////////////////////////////
 
+    /**
+     * Representa una tirada del dado
+     * 
+     * @param principal
+     * @return ruta a la que vamos
+     */
+    public String throwingDie(Principal principal) {
+        Player player = playerService.findPlayersByName(principal.getName());
+
+    }
 }
