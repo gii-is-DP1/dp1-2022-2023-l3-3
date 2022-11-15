14362d1d6450d7f1267ddb6c8d41abb9eb8c4235
María Vico <73558808+marvicmar@users.noreply.github.com>
2022-11-16 00:03:27 +0100

    Actualización de islas

--- src/main/java/sevenislands/game/island/Island.java
+++ src/main/java/sevenislands/game/island/Island.java
@@ -2,8 +2,11 @@ package sevenislands.game.island;
 
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.JoinTable;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
@@ -34,6 +37,5 @@ public class Island extends BaseEntity {
     @NotNull
     private Game game;
 
-    @ManyToMany(mappedBy = "islands")
-    private List<Card> cards;
+ 
 }
