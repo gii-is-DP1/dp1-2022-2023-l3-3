e83484537a945407599eeb8874c33f2a25a23efb
alepv1603 <alepervaz@us.es>
2022-11-17 00:26:15 +0100

    cambio en data sql

--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -20,38 +20,38 @@ INSERT INTO lobby(code,active) VALUES('aD5f8Lio','TRUE');
 INSERT INTO lobby_users(lobby_id,users_id) VALUES (1,8);
 INSERT INTO lobby_users(lobby_id,users_id) VALUES (1,9);
 
--- GAME -- 
+-- -- GAME -- 
 INSERT INTO game(id, creation_date, ending_date, lobby_id) VALUES (1, '2022-11-14', '2022-11-24', 1);
 
--- ROUNDS --
+-- -- ROUNDS --
 INSERT INTO round(game_id) VALUES (1);
 
--- TURNS -- 
+-- -- TURNS -- 
 INSERT INTO turn(user_id, round_id, start_time) VALUES (8,1, '2022-11-16 02:52:12');
 INSERT INTO turn(user_id, round_id, start_time) VALUES (9,1,'2022-11-16 18:52:12');
 
--- -- CARD --
---  INSERT INTO cards(tipo, name, multiplicity) VALUES ('Moneda', 'Doblon', 27);
---  INSERT INTO cards(tipo, name, multiplicity) VALUES ('Tesoro', 'Diamante', 3);
---  INSERT INTO cards(tipo, name, multiplicity) VALUES ('Tesoro', 'Rubi', 3);
---  INSERT INTO cards(tipo, name, multiplicity) VALUES ('Tesoro', 'Collar', 3);
+-- -- -- CARD --
+INSERT INTO card(tipo, name, multiplicity) VALUES ('Moneda', 'Doblon', 27);
+INSERT INTO card(tipo, name, multiplicity) VALUES ('Tesoro', 'Diamante', 3);
+INSERT INTO card(tipo, name, multiplicity) VALUES ('Tesoro', 'Rubi', 3);
+INSERT INTO card(tipo, name, multiplicity) VALUES ('Tesoro', 'Collar', 3);
 
--- -- -- ISLA --
--- INSERT INTO islands(island_number) VALUES (1);
--- INSERT INTO islands(island_number, game_id) VALUES (2,1);
--- INSERT INTO islands(island_number, game_id) VALUES (3,1);
--- INSERT INTO islands(island_number, game_id) VALUES (4,1);
--- INSERT INTO islands(island_number, game_id) VALUES (5,1);
--- INSERT INTO islands(island_number, game_id) VALUES (6,1);
+-- -- -- -- ISLA --
+INSERT INTO island(island_number) VALUES (1);
+INSERT INTO island(island_number,game_id) VALUES (2,1);
+INSERT INTO island(island_number, game_id) VALUES (3,1);
+INSERT INTO island(island_number, game_id) VALUES (4,1);
+INSERT INTO island(island_number, game_id) VALUES (5,1);
+INSERT INTO island(island_number, game_id) VALUES (6,1);
 
 -- -- -- CARTA_ISLA --
--- INSERT INTO cards_islands(card_id,islands_id) VALUES (1,1);
--- INSERT INTO cards_islands(card_id,islands_id) VALUES (1,2);
--- INSERT INTO cards_islands(card_id,islands_id) VALUES (2,3);
--- INSERT INTO cards_islands(card_id,islands_id) VALUES (3,4);
+INSERT INTO card_islands(card_id,islands_id) VALUES (1,1);
+INSERT INTO card_islands(card_id,islands_id) VALUES (1,2);
+INSERT INTO card_islands(card_id,islands_id) VALUES (2,3);
+INSERT INTO card_islands(card_id,islands_id) VALUES (3,4);
 
 
--- -- -- -- TURNO_CARTA --
--- INSERT INTO cards_turns(cards_id,turns_id) VALUES (1, 1);
--- INSERT INTO cards_turns(cards_id,turns_id) VALUES (1, 1);
--- INSERT INTO cards_turns(cards_id,turns_id) VALUES (1, 1);
+-- -- -- -- -- TURNO_CARTA --
+INSERT INTO turn_cards(turn_id,cards_id) VALUES (1, 1);
+INSERT INTO turn_cards(turn_id,cards_id) VALUES (1, 1);
+INSERT INTO turn_cards(turn_id,cards_id) VALUES (1, 1);
