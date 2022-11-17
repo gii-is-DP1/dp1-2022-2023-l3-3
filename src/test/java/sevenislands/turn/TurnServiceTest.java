684ac1b4f6102f112ce117aa253ee0be01b860ce
Sergio Santiago <sergiosantiago0403@gmail.com>
2022-11-17 03:00:33 +0100

    Service test lobby

--- src/test/java/sevenislands/turn/TurnServiceTest.java
+++ src/test/java/sevenislands/turn/TurnServiceTest.java
@@ -15,6 +15,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import sevenislands.game.turn.Turn;
 import sevenislands.game.turn.TurnRepository;
+import sevenislands.game.turn.TurnService;
 import sevenislands.user.User;
 
 @DataJpaTest
@@ -26,19 +27,22 @@ public class TurnServiceTest {
 
     @Test
     public void getAllTurnTest(){
-        List<Turn> listaTurnos = turnRepository.findAll();
+        TurnService turnService = new TurnService(turnRepository);
+        List<Turn> listaTurnos = turnService.findAllTurns();
         assertNotEquals(0, listaTurnos.size());
     }
 
     @Test
     public void getTurnByIdTest(){
-        Turn turno = turnRepository.findById(1).get();
+        TurnService turnService = new TurnService(turnRepository);
+        Turn turno = turnService.findTurnById(1).get();
         assertNotNull(turno);
     }
 
     @Test
     public void getTurnByRoundIdTest(){
-        List<Turn> turnos = turnRepository.findByRoundId(1);
+        TurnService turnService = new TurnService(turnRepository);
+        List<Turn> turnos = turnService.findByRoundId(1);
         assertNotNull(turnos);
     }
 
