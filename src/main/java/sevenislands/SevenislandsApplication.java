b5a659546c14fb14041ae5c1961b640f9038f88a
Juan Carlos Ramírez López <juancarlosralop@gmail.com>
2022-11-29 19:07:40 +0100

    Creación de logs

--- src/main/java/sevenislands/SevenislandsApplication.java
+++ src/main/java/sevenislands/SevenislandsApplication.java
@@ -1,12 +1,18 @@
 package sevenislands;
 
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication()
 public class SevenislandsApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
+		System.out.println("Aplicación iniciada...");
+		PrintStream printStream = new PrintStream("./logs/log.txt");
+		System.setOut(printStream);
 		SpringApplication.run(SevenislandsApplication.class, args);
 	}
 
