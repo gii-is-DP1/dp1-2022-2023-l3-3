76786a6ac83306d2f046575bb835f43c20e5850c
María <marvicmar@alum.us.es>
2022-12-11 12:31:08 +0100

    Limpieza de código Lobby

--- src/main/java/sevenislands/lobby/LobbyRepository.java
+++ src/main/java/sevenislands/lobby/LobbyRepository.java
@@ -1,6 +1,5 @@
 package sevenislands.lobby;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,18 +15,6 @@ public interface LobbyRepository extends CrudRepository<Lobby,Integer> {
     @Query("SELECT lobby FROM Lobby lobby WHERE lobby.code=?1")
 	public Optional<Lobby> findByCode(String code);
     
-    //No se usa en ningún lado
-    @Query(value = "SELECT lobby.id FROM Lobby lobby INNER JOIN lobby.users user WHERE user.id=?1")
-    public Integer findLobbyIdByPlayer(Integer player_id);
-
-    //No se usa en ningún lado
-    @Query(value = "SELECT lobby.id FROM Lobby lobby INNER JOIN lobby.users user WHERE user.nickname=?1 AND lobby.active=true")
-    public Optional<Integer> findLobbyByNicknamePlayer(String nickname);
-    
-    //No se usa en ningún lado
-    @Query("SELECT DISTINCT lobby FROM Lobby lobby where lobby.active=true")
-    public Collection<Lobby> findAllActiveLobby();
-
     @Query("SELECT lobby FROM Lobby lobby INNER JOIN lobby.users user WHERE user.id=?1")
     public Optional<Lobby> findByPlayerId(Integer user_id);
 }
