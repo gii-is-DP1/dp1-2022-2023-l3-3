b6671c6bdeb1395569fb9ea4ab6127dd6e00b06b
Sergio Santiago <sergiosantiago0403@gmail.com>
2022-12-12 12:53:31 +0100

    falta test round y turn

--- src/test/java/sevenislands/game/GameServiceTest.java
+++ src/test/java/sevenislands/game/GameServiceTest.java
@@ -2,7 +2,6 @@ package sevenislands.game;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
