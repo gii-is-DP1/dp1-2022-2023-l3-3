10ee3588448978293440977b50abb6311fea9f66
Juan Carlos Ramírez López <juancarlosralop@gmail.com>
2023-01-04 13:38:36 +0100

    Mejorada página de error

--- src/main/java/sevenislands/web/CrashController.java
+++ src/main/java/sevenislands/web/CrashController.java
@@ -3,17 +3,35 @@ package sevenislands.web;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import sevenislands.game.GameService;
+import sevenislands.lobby.LobbyService;
+import sevenislands.user.User;
 
 @Controller
 public class CrashController implements ErrorController{
 
+	private final LobbyService lobbyService;
+	private final GameService gameService;
+
+	@Autowired
+	public CrashController(LobbyService lobbyService, GameService gameService) {
+		this.lobbyService = lobbyService;
+		this.gameService = gameService;
+	}
+
 	@GetMapping("/error")
-	public String triggerException(ModelMap model, HttpServletRequest request) {
+	public String triggerException(ModelMap model, HttpServletRequest request, @ModelAttribute("logedUser") User logedUser) {
+		model.put("lobby", lobbyService.checkUserLobby(logedUser));
+		model.put("game", gameService.checkUserGame(logedUser));
+
 		Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 		if (status != null) {
 			int statusCode = Integer.parseInt(status.toString());
