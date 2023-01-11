c355f3303647764ed4e7f640dd314268d07cbd31
Juan Carlos Ramírez López <juancarlosralop@gmail.com>
2023-01-11 13:04:30 +0100

    GameService 45%

--- src/test/java/sevenislands/game/GameRepositoryTest.java
+++ src/test/java/sevenislands/game/GameRepositoryTest.java
@@ -20,7 +20,6 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import sevenislands.lobby.Lobby;
 
-@ExtendWith(SpringExtension.class)
 @DataJpaTest
 public class GameRepositoryTest {
 
@@ -40,12 +39,7 @@ public class GameRepositoryTest {
         game.setCreationDate(LocalDateTime.now());
         entityManager.persist(game);
         entityManager.flush();
-    
-
-        
         Optional<Game> foundGame = gameRepository.findGameByLobbyId(2);
-
-        
         assertTrue(foundGame.isPresent());
        
     }
@@ -63,97 +57,90 @@ public class GameRepositoryTest {
         game.setLobby(lobby);
         entityManager.persist(game);
         entityManager.flush();
-
-        
         Optional<Game> foundGame = gameRepository.findGameByLobby(lobby);
-
-        
         assertTrue(foundGame.isPresent());
         assertEquals(game, foundGame.get());
 }
 
-@Test
-public void testFindGamesByLobbies() {
-    
-    List<Lobby> lobbies = new ArrayList<>();
-    Lobby lobby1 = new Lobby();
-    lobby1.setCode("password");
-    entityManager.persist(lobby1);
-    entityManager.flush();
-    lobbies.add(lobby1);
+    @Test
+    public void testFindGamesByLobbies() {
+        
+        List<Lobby> lobbies = new ArrayList<>();
+        Lobby lobby1 = new Lobby();
+        lobby1.setCode("password");
+        entityManager.persist(lobby1);
+        entityManager.flush();
+        lobbies.add(lobby1);
 
-    Lobby lobby2 = new Lobby();
-    lobby2.setCode("password2");
-    entityManager.persist(lobby2);
-    entityManager.flush();
-    lobbies.add(lobby2);
+        Lobby lobby2 = new Lobby();
+        lobby2.setCode("password2");
+        entityManager.persist(lobby2);
+        entityManager.flush();
+        lobbies.add(lobby2);
 
-    Lobby lobby3 = new Lobby();
-    lobby3.setCode("password3");
-    entityManager.persist(lobby3);
-    entityManager.flush();
-    lobbies.add(lobby3);
+        Lobby lobby3 = new Lobby();
+        lobby3.setCode("password3");
+        entityManager.persist(lobby3);
+        entityManager.flush();
+        lobbies.add(lobby3);
 
-    
+        
 
-    List<Game> games = new ArrayList<>();
-    for (int i = 0; i < 3; i++) {
-        Game game = new Game();
-        game.setCreationDate(LocalDateTime.now());
-        game.setLobby(lobbies.get(i));
-        entityManager.persist(game);
-        games.add(game);
+        List<Game> games = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            Game game = new Game();
+            game.setCreationDate(LocalDateTime.now());
+            game.setLobby(lobbies.get(i));
+            entityManager.persist(game);
+            games.add(game);
+        }
+        entityManager.flush();
+
+        
+        Optional<List<Game>> foundGames = gameRepository.findGamesByLobbies(lobbies);
+
+        
+        assertTrue(foundGames.isPresent());
+        assertFalse(foundGames.get().isEmpty());
+
+        for (Game game : foundGames.get()) {
+            assertTrue(lobbies.contains(game.getLobby()));
+        }
     }
-    entityManager.flush();
 
-    
-    Optional<List<Game>> foundGames = gameRepository.findGamesByLobbies(lobbies);
+    @Test
+    public void findGameByLobbyAndActive_returnsGamesByLobbyAndActive() {
+        //Arrange
+        Lobby lobby1 = new Lobby();
+        lobby1.setCode("password1");
+        entityManager.persist(lobby1);
+        Lobby lobby2 = new Lobby();
+        lobby2.setCode("password2");
+        entityManager.persist(lobby2);
+        
+        Game game1 = new Game();
+        game1.setCreationDate(LocalDateTime.now());
+        game1.setLobby(lobby1);
+        game1.setActive(true);
+        entityManager.persist(game1);
 
-    
-    assertTrue(foundGames.isPresent());
-    assertFalse(foundGames.get().isEmpty());
+        Game game2 = new Game();
+        game2.setCreationDate(LocalDateTime.now());
+        game2.setLobby(lobby2);
+        game2.setActive(true);
+        entityManager.persist(game2);
 
-    for (Game game : foundGames.get()) {
-        assertTrue(lobbies.contains(game.getLobby()));
-    }
-}
+        List<Lobby> lobbies = Arrays.asList(lobby1, lobby2);
 
+        //Act
+        Optional<List<Game>> actualGames = gameRepository.findGameByLobbyAndActive(lobbies, true);
 
-
-@Test
-public void findGameByLobbyAndActive_returnsGamesByLobbyAndActive() {
-    //Arrange
-    Lobby lobby1 = new Lobby();
-    lobby1.setCode("password1");
-    entityManager.persist(lobby1);
-    Lobby lobby2 = new Lobby();
-    lobby2.setCode("password2");
-    entityManager.persist(lobby2);
-    
-    Game game1 = new Game();
-    game1.setCreationDate(LocalDateTime.now());
-    game1.setLobby(lobby1);
-    game1.setActive(true);
-    entityManager.persist(game1);
-
-    Game game2 = new Game();
-    game2.setCreationDate(LocalDateTime.now());
-    game2.setLobby(lobby2);
-    game2.setActive(true);
-    entityManager.persist(game2);
-
-    
-    List<Lobby> lobbies = Arrays.asList(lobby1, lobby2);
-
-    //Act
-    Optional<List<Game>> actualGames = gameRepository.findGameByLobbyAndActive(lobbies, true);
-
-    //Assert
-    assertTrue(actualGames.isPresent());
-    List<Game> games = actualGames.get();
-    assertEquals(2, games.size());
-    assertEquals(game1, games.get(1));
-    assertEquals(game2, games.get(0));
+        //Assert
+        assertTrue(actualGames.isPresent());
+        List<Game> games = actualGames.get();
+        assertEquals(2, games.size());
+        assertEquals(game1, games.get(1));
+        assertEquals(game2, games.get(0));
     }
 }
 
